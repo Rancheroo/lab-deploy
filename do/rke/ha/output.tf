7082b4969c50c8a939683a63bc14fb5625e9f8a3
Rancheroo <56899019+Rancheroo@users.noreply.github.com>
2019-12-09 15:42:08 +1100

    fixed indents

--- do/rke/ha/output.tf
+++ do/rke/ha/output.tf
@@ -2,20 +2,20 @@
 
 output "ha1_ip_output" {
   description = "IP addresses of HA1 droplet"
-  value       = data.digitalocean_droplet.ha1.ipv4_address
+  value       = digitalocean_droplet.ha1.ipv4_address
   }
 
 output "ha2_ip_output" { 
   description = "IP addresses of ha2 droplet"
-  value       = data.digitalocean_droplet.ha2.ipv4_address
+  value       = digitalocean_droplet.ha2.ipv4_address
   }
 
 output "ha3_ip_output" {
   description = "IP addresses of ha3 droplet"
-  value       = data.digitalocean_droplet.ha3.ipv4_address
+  value       = digitalocean_droplet.ha3.ipv4_address
   }
 
 output "ha4_ip_output" {
   description = "IP addresses of ha4 droplet"
-  value       = data.digitalocean_droplet.ha4.ipv4_address
+  value       = digitalocean_droplet.ha4.ipv4_address
  }
