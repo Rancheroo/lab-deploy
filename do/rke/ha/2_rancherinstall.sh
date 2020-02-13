2d8fa524438ac2a86239b1c2b6dd5912a3e7f297
Rancheroo <56899019+Rancheroo@users.noreply.github.com>
2020-02-13 18:12:50 +1100

    updated files

--- do/rke/ha/2_rancherinstall.sh
+++ do/rke/ha/2_rancherinstall.sh
@@ -2,5 +2,5 @@
 export KUBECONFIG=kube_config_cluster.yml
 helm install rancher rancher-stable/rancher \
   --namespace cattle-system \
-  --set hostname=brad.do.support.rancher.space
+  --set hostname=brad.do.support.rancher.space --version 2.3.4
 kubectl -n cattle-system rollout status deploy/rancher
